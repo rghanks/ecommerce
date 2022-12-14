8dba6dd51f17316c38d07aed4404858a68914534
rghanks <rghanks01@gmail.com>
2022-12-14 22:51:59 +0530

    backend completed

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,15 @@ app.use(cookieParser());
 
 // route imports
 
-// waching time 2:30
+// waching time 4:25
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use("/api/v1",product)
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 
 // middleware for errors
